79acb2890adc2bbb7434d59056e0d2ecd361b232
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-05-09 12:27:13 +0000

    [end]１８章　アプリケーションを完成させよう２（コメントの[投稿/削除]機能追加）

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
 
-  resources :post_images, only: [:new,:create,:index,:show,:destroy]
+  resources :post_images, only: [:new,:create,:index,:show,:destroy] do
+    resources :post_comments, only: [:create,:destroy]
+  end
   resources :user, only: [:show,:edit,:update]
-
-
 end
 
